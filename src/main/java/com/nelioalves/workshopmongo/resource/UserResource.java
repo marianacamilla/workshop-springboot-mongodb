a2a93bea466b24742f327609d13b6a019da0cd39
Mariana Camilla <marinkyouko@gmail.com>
2023-02-09 10:26:28 -0300

    Delete user with DELETE

--- src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
@@ -44,4 +44,10 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
