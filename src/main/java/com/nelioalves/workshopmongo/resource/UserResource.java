9101b5145140b3865be68928a6ff5ffb147f5277
Mariana Camilla <marinkyouko@gmail.com>
2023-02-09 10:22:11 -0300

    Insert user with POST

--- src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
@@ -1,17 +1,20 @@
 package com.nelioalves.workshopmongo.resource;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value="/users")
@@ -27,11 +30,18 @@ public class UserResource {
         return ResponseEntity.ok().body(listDto);
     }
 
-    @RequestMapping(value = "/{id}", method= RequestMethod.GET)
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
 }
