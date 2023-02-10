560cc87b8b6962c053f00adcc4375922552d78c5
Mariana Camilla <marinkyouko@gmail.com>
2023-02-10 08:16:54 -0300

    Simple query method search

--- src/main/java/com/nelioalves/workshopmongo/resource/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/PostResource.java
@@ -1,10 +1,13 @@
 package com.nelioalves.workshopmongo.resource;
 
 import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.resource.util.URL;
 import com.nelioalves.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -19,6 +22,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
